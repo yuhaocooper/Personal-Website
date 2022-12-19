25c53d9b03cf268462f5d5f9279df0c3138bfbb4
yuhao <tanyuhao@hotmail.com>
2022-12-20 01:09:42 +0800

    FE and BE changes

--- simple-node-server/index.js
+++ simple-node-server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config()
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require("mongoose");
@@ -24,10 +25,26 @@ mongoose.connect('mongodb://127.0.0.1:27017', options).then(() => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+//############################################################## CLIENT API ############################################################################
+app.get('/', async(req,res) =>{ 
+   // the responseMessage object extracts its values from environment variables
+   // If a value is not found, it instead stores the string "not found"
+   const responseMessage = {
+    environment: process.env.environment || "Not found",
+    apiBaseUrl: process.env.apiBaseUrl || "Not found"
+}
+
+res.send(responseMessage)
+})
+
 app.get('/details', async (req, res) => {
     // const user = await userModel.findOne({id: 123});
     const user = await userModel.find({});
     res.send(user);
 });
+
+
+//############################################################## CLIENT API ############################################################################
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
--- simple-react-app/src/App.js
+++ simple-react-app/src/App.js
@@ -5,16 +5,10 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          <button onClick={callApi}>Call API</button>
+          <body> "Yuhaocooper's site is under contruction. It will be up very shortly." </body>
         </header>
       </div>
   );
-}
-
-function callApi() {
-    fetch('http://localhost:4000/details', { method: 'GET' })
-        .then(response => response.json())
-        .then(data => alert(JSON.stringify(data)))
 }
 
 export default App;

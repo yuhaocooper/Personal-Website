25b2fe833d22ab0c846dafa805e2e25d8318dc36
yuhaocooper <yuhao@yuhaocooper.com>
2022-12-23 13:29:51 +0800

    local changes

--- Client/src/Second/Second.js
+++ Client/src/Second/Second.js
@@ -1,7 +1,20 @@
 import React from 'react';  
-   
+
+function fullname (user){
+    return user.first + user.last
+}
+
+const name = {
+    first: "yuhao",
+    last: " tan"
+}
+
+const element = (
+    <h1> Hello {fullname(name)}</h1>
+)
+
 function Home (){  
-    return <h1>Welcome to the 2nd page</h1>  
+    return element
 }  
    
 export default Home;  

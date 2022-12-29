73237aeb7e8e73cdf774b9f4744241d7615ec162
yuhao <tanyuhao@hotmail.com>
2022-12-29 09:55:18 +0800

    removed test BE route

--- Client/src/Pages/Home.js
+++ Client/src/Pages/Home.js
@@ -3,13 +3,6 @@ import About from './About.js';
 import Contact from './Contact.js';
 import Blog from './Blog.js'
 
-
-function callTestApi() {
-    fetch('http://localhost:4000/api/test')
-        .then(res => res.text())
-        .then(data => alert(data))
-}
-
 function Home (){  
     return (
         <div id='grid-container'>
@@ -25,7 +18,6 @@ function Home (){
                 <h1>HI, I'M YUHAO</h1>
                 <h1>HI, I'M YUHAO</h1>
                 <h2>blah blah blah</h2>
-                <button onClick={callTestApi}>Call Test API</button>
             </div>
             <div className='footer-container'>
                 <Footer></Footer>

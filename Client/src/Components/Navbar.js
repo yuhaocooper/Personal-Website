7b26e756c04a8e8432ffa6e11c66e703b7b64041
yuhaocooper <yuhao@yuhaocooper.com>
2023-01-02 17:54:26 +0800

    add / back to navbar

--- Client/src/Components/Navbar.js
+++ Client/src/Components/Navbar.js
@@ -5,6 +5,7 @@ function Header(props){
   return (
     <header>
       <nav className="header" style={{visibility: props.visibility}}>
+        <Link to="/">HOME</Link>|
         <Link to="/about">ABOUT</Link>|
         <Link to="/work">WORK</Link>|
         <Link to="/skills">SKILLS</Link>

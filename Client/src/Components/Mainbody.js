f708f1b59ebf80471ed54f5dfcfe66ed158122c9
yuhaocooper <yuhao@yuhaocooper.com>
2023-01-03 11:04:08 +0800

    changes to mainbody.js

--- Client/src/Components/Mainbody.js
+++ Client/src/Components/Mainbody.js
@@ -4,7 +4,8 @@ function Mainbody(){
 
     //use count as the number logic to switch between texts
     const [count, setCount] = useState(0)
-    const [title, setTitle] = useState(<div><div>Welcome!</div><span>HI, I'M YUHAO</span></div>)
+    const [title, setTitle] = useState()
+    const [subTitle, setSubTitle] = useState()
     const list = [0,1,2,3]
     
     //set interval to change count
@@ -37,11 +38,32 @@ function Mainbody(){
         }
     },[count])
 
+    useEffect(()=>{
+        if (count >3){
+            setCount(0)
+        }
+        switch(count){
+            case (0):
+                setSubTitle(<div className='sub-title'>WELCOME SUBTITLE</div>)
+                break;
+            case (1):
+                setSubTitle(<div className='sub-title'>ABOUT ME SUBTITLE</div>)
+                break;
+            case (2):
+                setSubTitle(<div className='sub-title'>WORK SUBTITLE</div>)
+                break;
+            case (3):
+                setSubTitle(<div className='sub-title'>SKILLS SUBTITLE</div>)
+                break;
+        }
+    },[count])
+
     //return the value
     // I need to split the png file into 2 separate layers at home
     return(
         <div>
             {title}
+            {subTitle}
             <div><a href="/about" className='testbutton'></a></div>
         </div>
     )

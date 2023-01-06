76c2ddf1674cb19ee83aa51cbc55fe9a8a2ac892
yuhao <tanyuhao@hotmail.com>
2023-01-06 23:29:44 +0800

    changes to work description

--- Client/src/Pages/Work.js
+++ Client/src/Pages/Work.js
@@ -8,17 +8,17 @@ function Work() {
     const content = [
         {
             title: "Pomelo",
-            description: <div><b style={{fontSize: '1.25rem'}}>Scrum Master</b><br></br>2022<br></br><br></br> Led agile transformation in the company <br></br><br></br> Works closely with squad to drive constant efficiency improvements and delivery of work to meet releases. <br></br><br></br> Coached teams and individuals to focus on value delivery and time-to-value.</div>,
+            description: <div><b style={{fontSize: '1.25rem'}}>Scrum Master</b><br></br>2022<br></br><br></br> Led agile transformation in the company. Project and product involved integrations with other Direct Acquirers, Card Schemes, PCI compliance, and Issuers. <br></br><br></br> Works closely with squad to drive constant efficiency improvements and delivery of work to meet releases. <br></br><br></br> Coached teams and individuals to focus on value delivery and time-to-value.</div>,
             cardName: "pomelo"
         },
         {
             title: "CrescoData",
-            description: <div><b style={{fontSize: '1.25rem'}}>Ecommerce Project Manager</b><br></br>2020-2022<br></br><br></br> Managed all eCommerce integration projects. <br></br><br></br> Led the integration for one of Australia's largest shopping malls. <br></br><br></br> Implemented Hybird project management principles. <br></br><br></br> Actively involved in requirements gathering and technical discussion calls with clients.</div>,
+            description: <div><b style={{fontSize: '1.25rem'}}>Ecommerce Project Manager</b><br></br>2020-2022<br></br><br></br> Managed all eCommerce integration projects. Platforms include Shopify, Bigcommerce, eBay, Lazada, Shopee, Catch, MySale and etc.<br></br><br></br> Led the integration for one of Australia's largest shopping malls. <br></br><br></br> Implemented Hybird project management principles. <br></br><br></br> Actively involved in requirements gathering and technical discussion calls with clients.</div>,
             cardName: "crescodata"
         },
         {
             title: "Style Theory",
-            description: <div> <b style={{fontSize: '1.25rem'}}>Logistics Lead</b><br></br>2018-2020<br></br><br></br> Built SG inhouse logistics service from scratch. <br></br><br></br> Directly managed a full fleet of drivers, supervisor and executive. <br></br><br></br> Launched secure unattended delivery service as a product feature to further reduce churn due to logistics issues.</div>,
+            description: <div> <b style={{fontSize: '1.25rem'}}>Logistics Lead</b><br></br>2018-2020<br></br><br></br> Built SG inhouse logistics service from scratch. Partnered with Ethoz Group to do so.<br></br><br></br> Directly managed a full fleet of drivers, supervisor and executive. <br></br><br></br> Launched secure unattended delivery service as a product feature to further reduce churn due to logistics issues.</div>,
             cardName: "styletheory"
         }
     ]
